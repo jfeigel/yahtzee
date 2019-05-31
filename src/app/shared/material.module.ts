fb14c8082131421019a7d2e1a81ca4a332e651f6
James Feigel <james.feigel@gmail.com>
2019-05-31 09:35:34 -0500

    Update to Angular Material v8

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatDialogModule,
-  MatToolbarModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
